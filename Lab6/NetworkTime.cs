b554ad5d3c428c218abbdc43bd817146e70cd88c
Mardari Sandu <alexmardari05@gmail.com>
2023-06-20 23:59:44 +0300

    first

---
+++ Lab6/NetworkTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    static class NetworkTime
+    {
+        public static DateTime GetNetworkTime()
+        {
+            var ntpServer = "pool.ntp.org";
+            var ntpData = new byte[48];
+            ntpData[0] = 0x1B;
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+
+                socket.ReceiveTimeout = 3000;
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
+            }
+
+            var intPart = (ulong)ntpData[40] << 24 | (ulong)ntpData[41] << 16 | (ulong)ntpData[42] << 8 | ntpData[43];
+            var fractPart = (ulong)ntpData[44] << 24 | (ulong)ntpData[45] << 16 | (ulong)ntpData[46] << 8 | ntpData[47];
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            return new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds((long)milliseconds);
+        }
+
+        public static string GetOffsetString(int offset)
+        {
+            if (offset >= 0)
+                return "+" + offset.ToString();
+            else
+                return offset.ToString();
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int offset = -12; offset <= 12; offset++)
+            {
+                DateTime time = NetworkTime.GetNetworkTime().AddHours(offset);
+                Console.WriteLine($"Exact time in GMT{NetworkTime.GetOffsetString(offset)}: {time}");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
